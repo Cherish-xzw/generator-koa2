8f6394b19dec45bcb1d0ccf895545bd3ebefc5cb
zhengwei.xu <zhengwei.xu@upchina.com>
2017-07-11 15:43:23 +0800

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const Generator = require('yeoman-generator');
+const path = require('path');
+
+module.exports = class extends Generator {
+
+    constructor(args, opts) {
+        super(args, opts);
+        this.sourceRoot(path.join(__dirname, '../templates'));
+    }
+
+    copyFiles() {
+        this.fs.copy(
+            this.templatePath(),
+            this.destinationPath()
+        );
+    }
+}
