5ab241f28d465438653614ec5f2088b82b348304
travisxu <xuzhengwei.amor@gmail.com>
2017-09-28 22:50:50 +0800

    Fix vue template with wrong package.json bug

--- app/index.js
+++ app/index.js
@@ -1,36 +1,29 @@
-const Generator = require('yeoman-generator');
-const path = require('path');
+const Generator = require("yeoman-generator");
+const path = require("path");
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.__template = this.options.vue ? "koa2-vue" : "koa2";
+  }
 
-    constructor(args, opts) {
-        super(args, opts);
-    }
-
-    copyFiles() {
-        if (this.options.vue) {
-            return this.fs.copy(
-                this.templatePath('koa2-vue/**/!(package.json)'),
-                this.destinationPath(), {
-                    globOptions: {
-                        dot: true
-                    }
-                }
-            );
+  copyFiles() {
+    this.fs.copy(
+      this.templatePath(this.__template + "/**/!(package.json)"),
+      this.destinationPath(),
+      {
+        globOptions: {
+          dot: true
         }
-        this.fs.copy(
-            this.templatePath('koa2/**/!(package.json)'),
-            this.destinationPath(), {
-                globOptions: {
-                    dot: true
-                }
-            }
-        );
-    }
+      }
+    );
+  }
 
-    replaceAppname() {
-        const pkg = this.fs.readJSON(this.templatePath('koa2/package.json'));
-        pkg.name = this.appname;
-        this.fs.writeJSON(this.destinationPath('package.json'), pkg);
-    }
-}
+  replaceAppname() {
+    const pkg = this.fs.readJSON(
+      this.templatePath(this.__template + "/package.json")
+    );
+    pkg.name = this.appname;
+    this.fs.writeJSON(this.destinationPath("package.json"), pkg);
+  }
+};
