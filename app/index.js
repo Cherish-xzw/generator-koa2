7075a38821e0cb3e6f5704eb4f8217dcd043f326
travisxu <xuzhengwei.amor@gmail.com>
2017-10-06 15:37:06 +0800

    Sync template from github repo

--- app/index.js
+++ app/index.js
@@ -1,10 +1,35 @@
 const Generator = require("yeoman-generator");
 const path = require("path");
+const download = require("download-git-repo");
+const ora = require("ora");
+const exists = require("fs").existsSync;
+const rm = require("rimraf").sync;
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.__template = this.options.vue ? "koa2-vue" : "koa2";
+    this.repo = this.options.vue
+      ? "Cherish-xzw/koa2-vue-boilerplate"
+      : "Cherish-xzw/koa2-boilerplate";
+  }
+
+  sync() {
+    const self = this;
+    const done = this.async();
+    const temp = this.templatePath(this.__template);
+    const spinner = ora("downloading tempaltes...");
+    spinner.start();
+    if (exists(temp)) rm(temp);
+    download(this.repo, temp, err => {
+      spinner.stop();
+      if (err) {
+        this.log(`Failed to download repo ${self.repo}: , ${err.message}`);
+        done(err);
+      }
+      this.log(`Success download repo to ${self.templatePath()}`);
+      done();
+    });
   }
 
   copyFiles() {
